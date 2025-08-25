a794e0b8c0c5bda2148b5e273e393bac86473863
Suhani Sahu <sahusuhani14@gmail.com>
2025-08-25 21:50:06 +0530

    changed api

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,29 @@ const app = express();
 
 // Middleware
 app.use(cors());
+
+app.use(cors({
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      "https://urvann-inky.vercel.app/",
+      "http://localhost:5173",
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow request
+    } else {
+      callback(new Error("Not allowed by CORS")); // Deny request
+    }
+  },
+  credentials: true, // Allow cookies to be sent
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+
+}));
+
 app.use(express.json());
 
 // Routes
 app.use("/api/plants", plantRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("🌱 Urvann backend is running successfully 🚀");
