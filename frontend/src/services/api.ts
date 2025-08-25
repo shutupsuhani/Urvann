af15a7c20084905501b21dd46b2869e3abcc6017
Suhani Sahu <sahusuhani14@gmail.com>
2025-08-25 21:40:16 +0530

    changed api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,21 +1,21 @@
-// src/services/api.ts or plantService.ts
+
 import axios from "axios";
 import { Plant } from "../types/Plant";
 
 // ✅ configure axios instance
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // adjust if hosted
+  baseURL: "https://urvann-backend.vercel.app/api", 
   timeout: 10000,
 });
 
 export const plantService = {
-  // ✅ Get all plants with optional filters
+  
   getPlants: async (params?: { name?: string; category?: string }) => {
     const response = await api.get<Plant[]>("/plants", { params });
     return response.data;
   },
 
-  // ✅ Get single plant by id
+  
   getPlant: async (id: string) => {
     const response = await api.get<Plant>(`/plants/${id}`);
     return response.data;
@@ -25,7 +25,7 @@ export const plantService = {
   addPlant: async (plantData: Omit<Plant, "_id" | "__v">) => {
     const response = await api.post<Plant>("/plants", plantData, {
       headers: {
-        "Content-Type": "application/json", // 👈 JSON now
+        "Content-Type": "application/json", 
       },
     });
     return response.data;
